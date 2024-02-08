4dafbcdc9c508aed35b1d3dd94daa43b3eeb9e47
r33anz <anzaldoalvaradorodrigo@gmail.com>
2024-02-07 23:44:50 -0400

    [ADD] Boton aniadir tarea y funcionalidad

---
+++ resources/views/productos/index.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        Listado de productos
+                        <a href="{{route('productos.create')}}" class="btn btn-success btn-sm float-right">Nuevo producto</a>
+                    </div>
+                    <div class="card-body">
+                        @if (session('info'))
+                            <div class="alert">
+                                {{session('info')}}
+                            </div>
+                        @endif
+                        <table class="table table-hover table-sm">
+                            <thead>
+                                <th> Descripcion</th>
+                                <th>Precio</th>
+                                
+                            </thead>
+                            <tbody>
+                                @foreach ($p as $prd)
+                                <tr>
+                                    <td>{{$prd -> descripcion}}</td>
+                                    <td>{{$prd -> precio}}</td>
+                                </tr>
+                                @endforeach   
+                                 
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Models\Product2;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('productos', function () {
+    $p =  Product2::all();
+    return view('productos.index',compact('p'));
+})-> name('productos.index');
+
+Route::get('productos/crear',function(){
+    return view('productos.create');
+}) -> name('productos.create');
+
+Route::post('productos',function(Request $request){
+    
+    $newP = new Product2;
+    $newP -> descripcion = $request -> input('descripcion');
+    $newP -> precio = $request -> input('precio');
+    $newP -> save();
+    return redirect() -> route('productos.index') -> with('info','Producto creado exitosamente');
+}) -> name('productos.guardar');
